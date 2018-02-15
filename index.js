9add0982d26e6a68b4b5b820b147aa6e172b60cf
Marisa Oppenheim <marisa.oppenheim@yahoo.com>
2018-02-15 18:45:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nowServing(katzDeliLine) {
     return 'There is nobody waiting to be served!'
   } else {
     var serve = katzDeliLine[0];
-      katzDeliLine.pop();
+      katzDeliLine.shift();
 
   }
   return 'Currently serving' + ' ' + serve + '.';
